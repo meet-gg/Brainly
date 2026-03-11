42a6a7337058b300810bfd8791b34195debe5708
meet-gg <meetgoyanip@gmail.com>
2026-03-11 18:52:27 +0530

    fix cors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import 'dotenv/config';
+// import 'dotenv/config';
 
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = 'https://brainly-fmmd.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
